33bdcca6952e3deccc68796a15927fb6529476a3
aotolbert@gmail.com <aotolbert@gmail.com>
2018-09-08 15:13:59 -0400

    functional bandsintown

--- liri.js
+++ liri.js
@@ -6,13 +6,13 @@ var dotenv = require('dotenv');
 dotenv.config()
 var moment = require("moment");
 var chalk = require("chalk");
+var fs = require("fs");
 
+var divider =
+"\n------------------------------------------------------------\n\n";
 
  
-var spotify = new Spotify({
-  id: "5968c001824a4a989de3c1c49ca1ac3c",
-  secret: "2467ad067dc748ff80f2f14c3a988d48"
-});
+
 
 if (process.argv[2] != "movie-this" && process.argv[2] != "spotify-this-song" && process.argv[2] != "concert-this" && process.argv[2] != "do-what-it-says") {
     console.log(`
@@ -40,9 +40,8 @@ if (process.argv[2] === "movie-this") {
 
 
 ]).then(function (userinput) {
-    var results = userinput
-    console.log(results)
-    var movietitle = JSON.stringify(results.QueryParam)
+
+    var movietitle = JSON.stringify(userinput.QueryParam)
     movietitle = movietitle.replace(" ", "+");
     console.log("Movie Title Query: " + movietitle);
     request(`http://www.omdbapi.com/?t=${movietitle}&y=&plot=short&limit=10&apikey=31c53d9`, function (error, response, body) {
@@ -52,6 +51,7 @@ if (process.argv[2] === "movie-this") {
     // console.log('body:', body); // Print the HTML for the Google homepage.
 
     var mymovies = JSON.parse(body);
+
     console.log(chalk.bold("Title: " + chalk.red(mymovies.Title)));
     console.log(chalk.bold("Rated: " + chalk.red(mymovies.Rated)));
     console.log(chalk.bold("Starring: " + chalk.red(mymovies.Actors)));
@@ -64,6 +64,11 @@ if (process.argv[2] === "movie-this") {
     
     // console.log(mymovies)
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    var logdata = `${userinput.Name} searched for: ${mymovies.Title} Rated: ${mymovies.Rated} Starring: ${mymovies.Actors} Released: ${mymovies.Released}`
+
+    fs.appendFile("log.txt", logdata + divider, function(err) {
+        if (err) throw err;
+      }); 
 
     
 })})};
@@ -71,13 +76,19 @@ if (process.argv[2] === "movie-this") {
 if (process.argv[2] === "spotify-this-song") {
     
         
-    inquirer.prompt(
-    {
-        type: "input",
-        name: "song",
-        message: "What song would you like to look up?"
-    }
-    ).then(function (userinput) {
+    var promptsong = function() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "Name",
+                message: "Please enter your name"
+            },
+            {
+                type: "input",
+                name: "song",
+                message: "What song would you like to look up?"
+            }
+        ]).then(function (userinput) {
         var iterator = 0;
         var querysong = JSON.stringify(userinput.song);
         querysong = querysong.replace(" ", "+");
@@ -90,6 +101,7 @@ if (process.argv[2] === "spotify-this-song") {
             if (err) {
             return console.log('Error occurred: ' + err);
             }
+
         var songartist  = (data.tracks.items[`${iterator}`].album.artists[`${iterator}`].name)
         var songalbum = (data.tracks.items[`${iterator}`].album.name);
         var songname = (data.tracks.items[`${iterator}`].name)
@@ -112,6 +124,13 @@ if (process.argv[2] === "spotify-this-song") {
                 if (isong.correctsong) {
                     console.log("Nice! Glad to hear you like that song. Its one of my favorites too!")
                     console.log(chalk.bgGreen(chalk.black("You can listen to that song here: " + chalk.bgRed(data.tracks.items[`${iterator}`].external_urls.spotify))));
+                    var logdata = `${userinput.Name} Searched for:${songname} By: ${songartist} Album: ${songalbum}`
+                    var divider =
+                    "\n------------------------------------------------------------\n\n";
+                   
+                    fs.appendFile("log.txt", logdata + divider, function(err) {
+                        if (err) throw err;
+                      }); 
                 } else {
                     iterator++
                     console.log("Oh No! That must be a more current song with a similar name, how about this one?")
@@ -130,11 +149,75 @@ if (process.argv[2] === "spotify-this-song") {
 
         });
 
-    })};
+    })}
+
+    promptsong();
+};
 
 
 if (process.argv[2] === "concert-this") {
 
+var promptconcert = function() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "Name",
+            message: "Please enter your name"
+        },
+        {
+            type: "input",
+            name: "QueryParam",
+            message: "Please enter the artist you want to look up"
+        }
+        
+        
+        ]).then(function (userinput) {
+            var artistquery = userinput.QueryParam.replace(" ", "%20")
 
-};
+            var URL = `https://rest.bandsintown.com/artists/${artistquery}/events?app_id=7bbb8d11c0827edee4d3e6fed11a78ef`;
+
+            request(URL, function(err, response, data) {
+                if (err) {
+                    console.log(`ERROR: ${err} Response Code: ${response}`)
+                } else{
+                    var jsondata = JSON.parse(data);
+                        if (jsondata.length === 0) {
+                            var errdata = `${userinput.Name} searched for: ${userinput} but gor an error <${response}>`
+                            console.log("Whoops, looks like this artist has no upcoming shows, try looking up someone else!")
+                            fs.appendFile("log.txt", errdata + divider, function(err) {
+                                if (err) throw err;
+                            });
+                            promptconcert();
+                        } else {
+                            for (i=0; i<jsondata.length; i++) {
+                            var thisconcert = jsondata[i]
+
+                            var newconcert = 
+                            `
+                            Venue: ${chalk.red(thisconcert.venue.name)}
+                            City, State: ${chalk.red(thisconcert.venue.city)}, ${chalk.red(thisconcert.venue.region)}
+                            Country: ${chalk.red(thisconcert.venue.country)}
+                            Time: ${chalk.red(thisconcert.datetime)}
+                            `;
+
+                            console.log(newconcert);
+                                if (thisconcert.offers.length > 0) {
+                                    console.log(`Buy Tickets Here: ${chalk.cyan(thisconcert.offers[0].url)}`);
+                                } else {
+                                    console.log(`ERROR: ${chalk.cyan("The url to purchase tickets is currently unavailible")}`);
+                                }
+                        
+                        }
+                        }
+                }
+
+
+            });
+
+
+        })};
+
+        promptconcert()
+    }
+
 
